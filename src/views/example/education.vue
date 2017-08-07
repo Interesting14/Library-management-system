d351a61d90ebd68a3e33a2b7df208d318b957fc5
GeSang-wu <742658977@qq.com>
2017-08-07 10:16:45 +0800

    2.0

--- src/views/example/education.vue
+++ src/views/example/education.vue
@@ -1,15 +1,35 @@
 <template>
 	<div class="app-container">
+
   		<el-table :data="tableData" border style="width: 100%" :default-sort = "{prop: 'date', order: 'descending'}">
-    		<el-table-column prop="id" label="编号" sortable width="180"></el-table-column>
-    		<el-table-column prop="image" label="封面"> 
-    			<!-- <img :src="img1" alt="" style="vertical-align: middle;"> -->
+    		<el-table-column prop="id" label="编号" sortable width="180" align="center"></el-table-column>
+    		<el-table-column  label="封面" align="center"> 
+    			<template scope="scope">
+    				<img :src="scope.row.image">
+    				<!-- 组件里面包含组件，作用域的问题，应用封装好的scope.row.方法 -->
+    				<!-- prop是渲染文本的 -->
+    			</template>
     		</el-table-column>
-    		<el-table-column prop="name" label="名称"></el-table-column>
-    		<el-table-column prop="author" label="作者"></el-table-column>
-    		<el-table-column prop="date" label="日期" sortable width="180"></el-table-column>
-    		<el-table-column prop="score" label="评分" sortable width="180"></el-table-column>
+    		<el-table-column prop="name" label="名称" align="center"></el-table-column>
+    		<el-table-column prop="author" label="作者" align="center"></el-table-column>
+    		<el-table-column prop="date" label="日期" sortable width="180" align="center"></el-table-column>
+    		<el-table-column prop="score" label="评分" sortable width="180" align="center"></el-table-column>
   		</el-table>
+
+  		<div class="block">
+    		<el-pagination 
+    			@size-change="handleSizeChange" 
+    			@current-change="handleCurrentChange" 
+    			:current-page="currentPage4" 
+    			:page-sizes="[10, 20, 30, 40]" 
+    			:page-size="10" 
+    			layout="total, sizes, prev, pager, next, jumper" 
+    			:total="400"
+    			style="margin-top: 30px;"
+    		>
+    		</el-pagination>
+  		</div>
+
 	</div>
 </template>
 
@@ -17,6 +37,15 @@
  	import {global} from 'src/global/global';
  	import {api} from 'src/global/api';
  	import img1 from 'assets/img/edu/edu0001.jpg';
+ 	import img2 from 'assets/img/edu/edu0002.jpg';
+ 	import img3 from 'assets/img/edu/edu0003.jpg';
+ 	import img4 from 'assets/img/edu/edu0004.jpg';
+ 	import img5 from 'assets/img/edu/edu0005.jpg';
+ 	import img6 from 'assets/img/edu/edu0006.jpg';
+ 	import img7 from 'assets/img/edu/edu0007.jpg';
+ 	import img8 from 'assets/img/edu/edu0008.jpg';
+ 	import img9 from 'assets/img/edu/edu0009.jpg';
+ 	import img10 from 'assets/img/edu/edu0010.jpg';
 
 
   	export default {
@@ -31,63 +60,63 @@
 	          		score:'9.3'	
 	        	}, {
 	          		id: 'edu0002',
-	        		image: '',
+	        		image: img2,
 	        		name: 'C程序设计语言',
 	        		author: '[美]Brian W. Kernighan',
 	          		date: '2004-01-01',
 	          		score:'9.4'
 	        	}, {
 	          		id: 'edu0003',
-	        		image: '',
+	        		image: img3,
 	        		name: '算法导论（原书第2版）',
 	        		author: '[美] Thomas H.Comen',
 	          		date: '2006-09-09',
 	          		score:'9.3'
 	        	}, {
 	          		id: 'edu0004',
-	        		image: '',
+	        		image: img4,
 	        		name: '算法（第4版）',
 	        		author: '[美]Nicholas C.Zakas',
 	          		date: '2012-03-29',
 	          		score:'9.3'
         		}, {
 	          		id: 'edu0005',
-	        		image: '',
+	        		image: img5,
 	        		name: 'Java编程思想',
 	        		author: '[美] Bruce Eckel',
 	          		date: '2007-06-03',
 	          		score:'9.1'
         		}, {
 	          		id: 'edu0006',
-	        		image: '',
+	        		image: img6,
 	        		name: '黑客与画家',
 	        		author: '[美] Paul Graham',
 	          		date: '2011-04-05',
 	          		score:'8.7'
         		}, {
 	          		id: 'edu0007',
-	        		image: '',
+	        		image: img7,
 	        		name: '集体智慧编程',
 	        		author: 'TOBY SEGARAN',
 	          		date: '2009-01-08',
 	          		score:'9.0'
         		}, {
 	          		id: 'edu0008',
-	        		image: '',
+	        		image: img8,
 	        		name: 'Python编程：从入门到实践',
 	        		author: '斯维加特',
 	          		date: '2016-07-01',
 	          		score:'8.7'
         		}, {
 	          		id: 'edu0009',
-	        		image: '',
+	        		image: img9,
 	        		name: '程序员的自我修养',
 	        		author: '俞甲子',
 	          		date: '2009-04-08',
 	          		score:'8.8'
         		}, {
 	          		id: 'edu0010',
-	        		image: '',
+	        		image: img10,
 	        		name: 'Fluent Python',
 	        		author: 'Andrew Hunt',
 	          		date: '2005-01-08',
--- src/views/example/youthLiterature.vue
+++ src/views/example/youthLiterature.vue
@@ -1,8 +1,132 @@
 <template>
-	<p>youthLiterature</p>
+	<div class="app-container">
+  		<el-table :data="tableData" border style="width: 100%" :default-sort = "{prop: 'date', order: 'descending'}">
+    		<el-table-column prop="id" label="编号" sortable width="180" align="center"></el-table-column>
+    		<el-table-column  label="封面" align="center"> 
+    			<template scope="scope">
+    				<img :src="scope.row.image">
+    				<!-- 组件里面包含组件，作用域的问题，应用封装好的scope.row.方法 -->
+    				<!-- prop是渲染文本的 -->
+    			</template>
+    		</el-table-column>
+    		<el-table-column prop="name" label="名称" align="center"></el-table-column>
+    		<el-table-column prop="author" label="作者" align="center"></el-table-column>
+    		<el-table-column prop="date" label="日期" sortable width="180" align="center"></el-table-column>
+    		<el-table-column prop="score" label="评分" sortable width="180" align="center"></el-table-column>
+  		</el-table>
+
+  		<div class="block">
+    		<el-pagination 
+    			@size-change="handleSizeChange" 
+    			@current-change="handleCurrentChange" 
+    			:current-page="currentPage4" 
+    			:page-sizes="[10, 20, 30, 40]" 
+    			:page-size="10" 
+    			layout="total, sizes, prev, pager, next, jumper" 
+    			:total="400"
+    			style="margin-top: 30px;"
+    		>
+    		</el-pagination>
+  		</div>
+  		
+	</div>
 </template>
 
 <script>
-	import {global} from 'src/global/global';
-	import {api} from 'src/global/api';
+ 	import {global} from 'src/global/global';
+ 	import {api} from 'src/global/api';
+ 	import img1 from 'assets/img/yth/yth0001.jpg';
+ 	import img2 from 'assets/img/yth/yth0002.jpg';
+ 	import img3 from 'assets/img/yth/yth0003.jpg';
+ 	import img4 from 'assets/img/yth/yth0004.jpg';
+ 	import img5 from 'assets/img/yth/yth0005.jpg';
+ 	import img6 from 'assets/img/yth/yth0006.jpg';
+ 	import img7 from 'assets/img/yth/yth0007.jpg';
+ 	import img8 from 'assets/img/yth/yth0008.jpg';
+ 	import img9 from 'assets/img/yth/yth0009.jpg';
+ 	import img10 from 'assets/img/yth/yth0010.jpg';
+
+
+  	export default {
+    	data() {
+      		return {
+        		tableData: [{
+	        		id: 'yth0001',
+	        		image: img1,
+	        		name: '挪威的森林',
+	        		author: '[日] 村上春树',
+	          		date: '2001-03-05',
+	          		score:'8.0'	
+	        	}, {
+	          		id: 'yth0002',
+	        		image: img2,
+	        		name: '最好的我们',
+	        		author: '八月长安',
+	          		date: '2013-08-05',
+	          		score:'8.7'
+	        	}, {
+	          		id: 'yth0003',
+	        		image: img3,
+	        		name: '一个人的好天气',
+	        		author: '[日] 青山七惠',
+	          		date: '2007-09-10',
+	          		score:'7.6'
+	        	}, {
+	          		id: 'yth0004',
+	        		image: img4,
+	        		name: '此间的少年',
+	        		author: '江南',
+	          		date: '2004-01-29',
+	          		score:'8.3'
+        		}, {
+	          		id: 'yth0005',
+	        		image: img5,
+	        		name: '动物凶猛',
+	        		author: '王朔',
+	          		date: '2004-02-03',
+	          		score:'8.2'
+        		}, {
+	          		id: 'yth0006',
+	        		image: img6,
+	        		name: '余生，请多指教',
+	        		author: '柏林石匠',
+	          		date: '2016-10-05',
+	          		score:'7.5'
+        		}, {
+	          		id: 'yth0007',
+	        		image: img7,
+	        		name: '何以笙箫默',
+	        		author: '顾漫',
+	          		date: '2007-04-08',
+	          		score:'8.0'
+        		}, {
+	          		id: 'yth0008',
+	        		image: img8,
+	        		name: '少年巴比伦',
+	        		author: '路内',
+	          		date: ' 2008-08-01',
+	          		score:'8.5'
+        		}, {
+	          		id: 'yth0009',
+	        		image: img9,
+	        		name: '坏一坏',
+	        		author: '凉炘',
+	          		date: '2017-04-01',
+	          		score:'8.6'
+        		}, {
+	          		id: 'yth0010',
+	        		image: img10,
+	        		name: '你是最好的自己',
+	        		author: '杨杨',
+	          		date: '2014-03-10',
+	          		score:'7.2'
+        		}]
+      		}
+    	},
+    	methods: {
+      		formatter(row, column) {
+        		return row.address;
+      		}
+    	}
+  	}
 </script>
