974785c5e2588da7cd4acc98e0e6d831c49f47df
GeSang-wu <742658977@qq.com>
2017-08-10 09:58:06 +0800

    remove dir

--- src/views/example/recommendBook.vue
+++ src/views/example/recommendBook.vue
@@ -1,5 +1,15 @@
 <template>
-	<p>recommendBook</p>
+<div class="app-container">
+	<el-tooltip placement="bottom">
+  		<div slot="content">
+  			名称：你是最好的自己<br /><br />
+  			作者：杨杨<br /><br />
+  			出版日期：2014-03-10<br /><br />
+  			评分：8.6
+  		</div>
+  		<el-button>Top center</el-button>
+	</el-tooltip>
+</div>
 </template>
 
 <script>
